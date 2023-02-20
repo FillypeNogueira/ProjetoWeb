878a9f69095baa1ba01ca954552af0c81871bf22
Fillype Nogueira Vieira <fillypenv@gmail.com>
2023-02-19 22:04:58 -0300

    Estrutura inicial

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Projeto_Sistema_WEB.Controllers
 
 {
-    public class HomeController 
+    public class HomeController : Controller
     {
-        
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "";
+            ViewData["email"] = "";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+        }
+
+        public IActionResult Privacy()
+        {
+            
+        }
+
+
+
+
     }
 }
