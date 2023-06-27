d2264d2310b18cda1e2eb03e46c9f3e087cc7012
Fillype Nogueira Vieira <fillypenv@gmail.com>
2023-06-27 00:04:41 -0300

    Alteração na entidade Cliente, adição do campo CPF

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -3,18 +3,22 @@ namespace Projeto_Sistema_WEB.Model
     public class Cliente
     {
         public int Id {get; set; }
+
         public string Nome { get; set; }
 
+        public string CPF {get; set;}
+        
         public string Endereco { get; set; }
 
         public string Telefone { get; set; }
 
         public string Email {get; set;}
 
-        public Cliente (int id, string nome, string endereco, string telefone, string email)
+        public Cliente (int id, string nome, string cpf, string endereco, string telefone, string email)
         {
             Id = id;
             Nome = nome;
+            CPF = cpf;
             Endereco = endereco;
             Telefone = telefone;
             Email = email;
